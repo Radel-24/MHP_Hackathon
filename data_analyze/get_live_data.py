a3a05adeb765a6aea9cd3e4edd56e3963e20491d
Robin Adelwarth <radelwar@1-E-13.42heilbronn.de>
2022-03-04 20:25:51 +0100

    csv writer

--- data_analyze/get_live_data.py
+++ data_analyze/get_live_data.py
@@ -1,6 +1,8 @@
 
 # argv[1] is name of sumo config file
 
+import csv
+import time
 import os
 import sys
 import traci
@@ -9,6 +11,7 @@ import sumolib
 import sumolib.miscutils
 from sumolib.miscutils import getFreeSocketPort
 from sumolib import checkBinary
+
 
 if 'SUMO_HOME' in os.environ:
 	tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
@@ -29,14 +32,40 @@ vehID = 'left_0'
 traci.vehicle.subscribe(vehID, (tc.VAR_ROAD_ID, tc.VAR_LANEPOSITION))
 print(traci.vehicle.getSubscriptionResults(vehID))
 
-# while traci.simulation.getMinExpectedNumber() > 0:
-# 	traci.simulationStep()
-# 	print(traci.vehicle.getSubscriptionResults(vehID))
-for i in range(3):
+fieldnames = ["timestamp", "CO2", "fuel", "noise", "standing_cars"]
+
+with open('data.csv', 'w') as csv_file:
+	csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+	csv_writer.writeheader()
+
+
+i = 0
+while traci.simulation.getMinExpectedNumber() > 0:
 	traci.simulationStep()
-	print("step:" + str(i))
-	print(traci.vehicle.getSubscriptionResults(vehID))
-	print("CO2: " + str(traci.vehicle.getCO2Emission(vehID)))
+	#print("step:" + str(i))
+	i += 1
+	#print(traci.vehicle.getSubscriptionResults(vehID))
+	#print("CO2: " + str(traci.vehicle.getCO2Emission(vehID)))
+	info = {
+		"timestamp": i,
+		"CO2": traci.vehicle.getCO2Emission(vehID),
+		"fuel": traci.vehicle.getFuelConsumption(vehID),
+		"noise": traci.vehicle.getNoiseEmission(vehID),
+		"standing_cars": 42
+	}
+
+	csv_writer.writerow(info)
+	#print(x_value, total_1, total_2, total_3, total_4)
+
+	time.sleep(1)
+
+
+	 #print(traci.vehicle.getSubscriptionResults(vehID))
+#for i in range(3):
+#	traci.simulationStep()
+#	print("step:" + str(i))
+#	print(traci.vehicle.getSubscriptionResults(vehID))
+#	print("CO2: " + str(traci.vehicle.getCO2Emission(vehID)))
 
 
 
