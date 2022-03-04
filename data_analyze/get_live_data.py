55117f86e1a6a9a7c7db7ed1836f9adcc0b41fa0
Robin Adelwarth <robin.adelwarth@web.de>
2022-03-04 12:08:03 -0500

    running getting data of one car

--- data_analyze/get_live_data.py
+++ data_analyze/get_live_data.py
@@ -5,7 +5,10 @@ import os
 import sys
 import traci
 import traci.constants as tc
+import sumolib
+import sumolib.miscutils
 from sumolib.miscutils import getFreeSocketPort
+from sumolib import checkBinary
 
 if 'SUMO_HOME' in os.environ:
 	tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
@@ -16,20 +19,24 @@ else:
 freePort = sumolib.miscutils.getFreeSocketPort()
 
 sumoBinary = checkBinary('sumo-gui') # change to 'sumo' when running without gui
-sumoCmd = [sumoBinary, "-c", sys.argv[1], "--num-clients", "1"], port=freePort
+traci.start([sumoBinary, "-c", sys.argv[1], "--num-clients", "1", "--emission-output", "emi.xml"], port=freePort)
 
-traci.start(sumoCmd)
+# traci.start(sumoCmd)
 traci.setOrder(1)
 
-vehID = 'left_204'
+vehID = 'left_0'
 
 traci.vehicle.subscribe(vehID, (tc.VAR_ROAD_ID, tc.VAR_LANEPOSITION))
 print(traci.vehicle.getSubscriptionResults(vehID))
 
-while traci.simulation.getMinExpectedNumber() > 0:
+# while traci.simulation.getMinExpectedNumber() > 0:
+# 	traci.simulationStep()
+# 	print(traci.vehicle.getSubscriptionResults(vehID))
+for i in range(3):
 	traci.simulationStep()
+	print("step:" + str(i))
 	print(traci.vehicle.getSubscriptionResults(vehID))
-
+	print("CO2: " + str(traci.vehicle.getCO2Emission(vehID)))
 
 
 
