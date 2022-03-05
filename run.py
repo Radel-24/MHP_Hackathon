979391978c9ab56002e9f4d71980a264b53ffa21
Robin Adelwarth <robin.adelwarth@web.de>
2022-03-05 06:43:34 -0500

    save

--- run.py
+++ run.py
@@ -1,4 +1,30 @@
 import subprocess
+import os
+import sys
+import sumolib
+import traci
+from sumolib import checkBinary
+from sumolib.miscutils import getFreeSocketPort
 
-subprocess.call(["./data_analyze/get_live_data.py", "../../sumo/tests/complex/tutorial/traci_tls/data/cross.sumocfg"])
-subprocess.call("./sumoVisualizer/graph_visualizer.py")
+if 'SUMO_HOME' in os.environ:
+	tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+	sys.path.append(tools)
+else:
+	sys.exit("environment variable 'SUMO_HOME' not set")
+
+freePort = sumolib.miscutils.getFreeSocketPort()
+
+sumoBinary = checkBinary('sumo-gui') # change to 'sumo' when running without gui
+traci.start([sumoBinary, "-c", sys.argv[1], "--num-clients", "1", "--emission-output", "emi.xml"], port=freePort)
+
+# traci.start(sumoCmd)
+traci.setOrder(1)
+
+
+subprocess.call(["python", "data_analyze/get_live_data.py", str(freePort), str(2)])
+# subprocess.call("./sumoVisualizer/graph_visualizer.py", 3)
+
+while traci.simulation.getMinExpectedNumber() > 0:
+    traci.simulationStep()
+
+traci.close()
