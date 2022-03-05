94514ca3782f68f9d125290694cd816e7535cd5e
Robin Adelwarth <robin.adelwarth@web.de>
2022-03-05 04:33:57 -0500

    save

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import subprocess
 
-subprocess.call(["data_analyze/get_live_data.py", "../../sumo/tests/complex/tutorial/traci_tls/data/cross.sumocfg"])
+subprocess.call(["./data_analyze/get_live_data.py", "../../sumo/tests/complex/tutorial/traci_tls/data/cross.sumocfg"])
 subprocess.call("./sumoVisualizer/graph_visualizer.py")
